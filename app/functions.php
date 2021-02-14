19e9b341c54c63a7713445168484359c2f7da337
Rodion Kachkin <kyleRQWS@gmail.com>
2021-02-14 19:37:41 +0400

    Refactor html code in welcome function

--- app/functions.php
+++ app/functions.php
@@ -45,10 +45,10 @@ function welcome(): void
 <head>
     <meta charset="utf-8">
     <title>Qunity Welcome</title>
+    <link href="http://fonts.cdnfonts.com/css/trivial" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
             integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
             crossorigin="anonymous"></script>
-    <link href="http://fonts.cdnfonts.com/css/trivial" rel="stylesheet">
     <style>
         * {
             -webkit-touch-callout: none;
