dffe9ff3eb82f135110d857e2a75230d65734a44
Rodion Kachkin <kyleRQWS@gmail.com>
2021-03-30 09:23:42 +0400

    Delete favicon.ico and create favicon.svg, refactor "welcome" function

--- app/functions.php
+++ app/functions.php
@@ -18,10 +18,10 @@ function init(): void
 
     $composer = json_decode((string)file_get_contents(BASE_DIR . '/composer.json'), true);
     define('PROJECT_VERSION', $composer['version'] ?? null);
-    define('PROJECT_AUTHORS', $composer['authors'] ?? []);
     define('PROJECT_TIME', $composer['time'] ?? null);
 
-    define('PROJECT_MODE', getenv('PROJECT_MODE'));
+    define('PROJECT_NAME', (string)getenv('PROJECT_NAME'));
+    define('PROJECT_MODE', (string)getenv('PROJECT_MODE'));
 }
 
 /**
@@ -29,14 +29,15 @@ function init(): void
  */
 function welcome(): void
 {
+    $project = PROJECT_NAME;
     if (PHP_SAPI == 'cli') {
-        $info = (PROJECT_VERSION != null ? " version \e[33m" . PROJECT_VERSION . "\e[0m" : '') .
-            (PROJECT_TIME != null ? ' ' . PROJECT_TIME : '');
-        print "\e[32mQunity\e[0m" . ($info ?: ' Welcome') . "\n";
+        $info = ((PROJECT_VERSION != null ? " version \e[33m" . PROJECT_VERSION . "\e[0m" : '') .
+            (PROJECT_TIME != null ? ' ' . PROJECT_TIME : '')) ?: ' Welcome';
+        $content = "\e[32m" . $project . "\e[0m" . $info . "\n";
     } else {
-        $info = (PROJECT_VERSION != null ? 'v.' . PROJECT_VERSION : '') .
-            (PROJECT_TIME != null ? ' ' . PROJECT_TIME : '');
-        print preg_replace(
+        $info = ((PROJECT_VERSION != null ? 'v.' . PROJECT_VERSION : '') .
+            (PROJECT_TIME != null ? ' ' . PROJECT_TIME : '')) ?: 'welcome';
+        $content = preg_replace(
             ['%>(\s)+%s', '%(\s)+<%s', '%(\s)+%s'],
             ['>', '<', '\\1'],
             <<<HTML
@@ -44,12 +45,20 @@ function welcome(): void
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Qunity Welcome</title>
-    <link href="http://fonts.cdnfonts.com/css/trivial" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
-            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-            crossorigin="anonymous"></script>
+    <title>{$project}</title>
+    <link rel="icon" type="image/svg+xml" href="favicon.svg">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <style>
+        @font-face { /* variants: https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap */
+          font-family: 'Jost';
+          font-style: normal;
+          font-weight: 300;
+          font-display: swap;
+          src: url(https://fonts.gstatic.com/s/jost/v6/92zPtBhPNqw79Ij1E865zBUv7mz9JTVBNIg.woff2) format('woff2');
+          unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F,
+            U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        }
         * {
             -webkit-touch-callout: none;
               -webkit-user-select: none;
@@ -66,42 +75,48 @@ function welcome(): void
             display: flex;
             align-items: center;
             justify-content: center;
-            height: 70%;
+            height: 75%;
         }
         .welcome > div {
             opacity: 0;
-            font-family: 'Trivial', sans-serif;
-            font-weight: bold;
+            text-align: center;
+            line-height: 1.2em;
+            font-family: 'Jost', sans-serif;
         }
         .title {
             text-transform: uppercase;
-            font-size: 98px;
-            color: #808080;
+            letter-spacing: 0.6em;
+            word-spacing: 0.2em;
+            font-size: 5em;
+            color: #999;
         }
         .info {
-            margin-top: 25px;
-            text-align: center;
-            font-size: 12px;
+            text-transform: lowercase;
+            letter-spacing: 0.1em;
+            word-spacing: 0.5em;
+            font-size: 0.8em;
             color: #ccc;
         }
     </style>
 </head>
 <body>
-<div class="wrapper">
-    <div class="welcome">
-        <div class="title">q&nbsp;u&nbsp;n&nbsp;i&nbsp;t&nbsp;y</div>
-        <div class="info">{$info}</div>
-        <script>
-            $(function () {
-                $('.title').fadeTo(250, 1).next('.info').delay(750).fadeTo('slow', 1);
-            });
-        </script>
+    <div class="wrapper">
+        <div class="welcome">
+            <div class="title">{$project}</div>
+            <div class="info">{$info}</div>
+            <script>
+                $(function () {
+                    $('.title').fadeTo(250, 1).next('.info').delay(750).fadeTo('slow', 1);
+                });
+            </script>
+        </div>
     </div>
 </body>
 </html>
 HTML
         );
     }
+    print $content;
 }
 
 /**
