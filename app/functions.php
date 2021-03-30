783e59a47e1c63b7caff529bcf2d494705a94a4b
Rodion Kachkin <kyleRQWS@gmail.com>
2021-03-30 09:44:32 +0400

    Fix html in "welcome" function

--- app/functions.php
+++ app/functions.php
@@ -51,12 +51,12 @@ function welcome(): void
             integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <style>
         @font-face { /* variants: https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap */
-          font-family: 'Jost';
-          font-style: normal;
-          font-weight: 300;
-          font-display: swap;
-          src: url(https://fonts.gstatic.com/s/jost/v6/92zPtBhPNqw79Ij1E865zBUv7mz9JTVBNIg.woff2) format('woff2');
-          unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F,
+            font-family: 'Jost';
+            font-style: normal;
+            font-weight: 300;
+            font-display: swap;
+            src: url(https://fonts.gstatic.com/s/jost/v6/92zPtBhPNqw79Ij1E865zBUv7mz9JTVBNIg.woff2) format('woff2');
+            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F,
             U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
         }
         * {
