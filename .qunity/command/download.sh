b9b815c734bcaa8f9c17c5a968e0a674a11e2a02
Rodion Kachkin <r.kachkin@mygento.com>
2023-06-27 23:24:40 +0400

    Update script qunity.sh

---
+++ .qunity/command/download.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+function description() {
+  echo "Downloads repositories from list repositories file"
+}
+
+function help() {
+  echo -e "$(color 32 "$(description)")
+
+$(color 33 "Usage:")
+    command [options]
+
+$(color 33 "Options:")
+    -h, --help\t\t- Display this help menu
+    -f, --force\t\t- Forced download of repositories
+\t\t\t  WARNING: Will delete all previously unsaved data"
+}
+
+function execute() {
+  while IFS=' ' read -r REPOSITORY RELATIVEPATH; do
+    if [[ -z "$REPOSITORY" || -z "$RELATIVEPATH" ]]; then continue; fi
+
+    local REPLACE_DIR
+    REPLACE_DIR="$(realpath --canonicalize-missing "${QUNITY_DIR}/replace/${RELATIVEPATH}")"
+
+    local DESTINATION_DIR
+    DESTINATION_DIR="$(realpath --canonicalize-missing "${BASE_DIR}/${RELATIVEPATH}")"
+
+    if [[ "${1-}" == '-f' || "${1-}" == '--force' ]]; then rm -rf "$DESTINATION_DIR"; fi
+    git clone --single-branch "$REPOSITORY" "$DESTINATION_DIR"
+    if [[ -d "$REPLACE_DIR" ]]; then cp -r "$REPLACE_DIR" "$(dirname "$DESTINATION_DIR")"; fi
+  done < "${QUNITY_DIR}/repositories"
+}
--- qunity.sh
+++ qunity.sh
@@ -3,7 +3,11 @@
 set -o nounset
 set -o errexit
 
+readonly SCRIPT_VERSION="v0.1.0-dev"
+
 readonly BASE_DIR="$(realpath "$(dirname "$0")")"
+readonly QUNITY_DIR="${BASE_DIR}/.qunity"
+
 source "${BASE_DIR}/.env"
 
 # Change first letter to upper
@@ -11,23 +15,69 @@ function ucfirst() {
   echo -n "${1:0:1}" | tr '[:lower:]' '[:upper:]'; echo "${1:1}"
 }
 
-# Success message
-function print() {
-  echo -e "$(date +'%T') \033[32m> $(ucfirst "$1")\033[0m"; return 0
+# Wrap string to color
+function color() {
+  echo "\033[${1}m${2}\033[0m"
 }
 
-# Failure message
+# Print script message
+function print() {
+  echo -e "$(date +'%T') $(color "${2-0}" "$(ucfirst "$1")")"
+}
+
+# Save failure message of result
 function error() {
-  echo -e "$(date +'%T') \033[31m> $(ucfirst "$1")\033[0m"; return 1
+  export RESULT_MESSAGE="ERROR: $1"; return 1
+}
+
+# Save information message of result
+function info() {
+  export RESULT_MESSAGE="INFO: $1"; return 0
+}
+
+# Display main help
+function help() {
+  echo -e "$(color 32 "Qunity ${SCRIPT_VERSION}")
+
+$(color 33 "Usage:")
+    command [options] [arguments]
+
+$(color 33 "Options:")
+    -h, --help\t\t- Display this help menu
+
+$(color 33 "Commands:")
+$(while IFS=' ' read -r COMMAND_FILE; do
+  local COMMAND="${COMMAND_FILE%".sh"}"
+  local COMMAND_FILE="${QUNITY_DIR}/command/${COMMAND_FILE}"
+
+  # shellcheck source=/.qunity/command/*.sh
+  source "$COMMAND_FILE"; echo -e "    ${COMMAND}\t\t- $(description)"
+done < <(ls "${QUNITY_DIR}/command"))"
 }
 
 # Execute callback function
-function call() {
-  if "${@: 1:$#-2}"; then print "${@: -2:1}"; else error "${@: -1:1}"; fi
+function execute() {
+  "${@}"
 }
 
-print "Start $([ -n "${*:1}" ] && echo "'${*:1}'" || echo "")"
+# Start script executing
+function start() {
+  local COMMAND=( "$@" ) START_MESSAGE=( "$@" )
+  local COMMAND_FILE="${QUNITY_DIR}/command/${COMMAND[0]-"unexist"}.sh"
 
-case "${1-}" in
-  *) call "${@:1}" "Success" "Runtime error" ;;
-esac
+  # shellcheck source=/.qunity/command/*.sh
+  if [[ -f "$COMMAND_FILE" ]]; then source "$COMMAND_FILE"; unset 'COMMAND[0]'; fi
+
+  local FUNCTION=execute; for PARAMETER in "${COMMAND[@]}"; do
+    if [[ "$PARAMETER" == '-h' || "$PARAMETER" == '--help' ]]; then FUNCTION=help; fi
+  done
+
+  if [[ $# -eq 0 || "$FUNCTION" == help ]]; then help; return 0; fi
+
+  print "INFO: Start '${START_MESSAGE[*]}'" 32; if execute "${COMMAND[@]}"
+    then print "${RESULT_MESSAGE="INFO: Success"}" 32
+    else print "${RESULT_MESSAGE="ERROR: Unknown"}" 31
+  fi
+}
+
+start "${@:1}"
