586f9449c88e6e62ee9303ab38b6a5e91b363f6d
Rodion Kachkin <r.kachkin@mygento.com>
2023-06-29 00:14:02 +0400

    Update qunity.sh script to v0.1.1-dev

--- qunity.sh
+++ qunity.sh
@@ -3,12 +3,14 @@
 set -o nounset
 set -o errexit
 
-readonly SCRIPT_VERSION="v0.1.0-dev"
+readonly SCRIPT_VERSION="v0.1.1-dev"
 
 readonly BASE_DIR="$(realpath "$(dirname "$0")")"
 readonly QUNITY_DIR="${BASE_DIR}/.qunity"
+readonly ENV_FILE="${BASE_DIR}/.env"
 
-source "${BASE_DIR}/.env"
+# shellcheck source=./.env
+if [[ -f "$ENV_FILE" ]]; then source "$ENV_FILE"; fi
 
 # Change first letter to upper
 function ucfirst() {
@@ -46,38 +48,41 @@ $(color 33 "Options:")
     -h, --help\t\t- Display this help menu
 
 $(color 33 "Commands:")
-    $(while IFS=' ' read -r COMMAND_FILE; do
-      local COMMAND_NAME="${COMMAND_FILE%".sh"}"
-      local COMMAND_FILE="${QUNITY_DIR}/command/${COMMAND_FILE}"
-
-      # shellcheck source=./.qunity/command/*.sh
-      source "$COMMAND_FILE"; echo -e "${COMMAND_NAME}\t\t- $(description)"
-    done < <(ls "${QUNITY_DIR}/command"))"
-}
-
-# Execute callback function
-function execute() {
-  "${@}"
-}
-
-# Start script executing
-function start() {
-  local COMMAND=( "$@" ) START_MESSAGE=( "$@" )
-  local COMMAND_FILE="${QUNITY_DIR}/command/${COMMAND[0]-"unexist"}.sh"
+$(while IFS=' ' read -r COMMAND_FILE; do
+  local COMMAND_NAME="${COMMAND_FILE%".sh"}"
+  local COMMAND_FILE="${QUNITY_DIR}/command/${COMMAND_FILE}"
 
   # shellcheck source=./.qunity/command/*.sh
-  if [[ -f "$COMMAND_FILE" ]]; then source "$COMMAND_FILE"; unset 'COMMAND[0]'; fi
+  source "$COMMAND_FILE"
+  echo -e "    ${COMMAND_NAME}\t\t- $(description)"
+done < <(ls "${QUNITY_DIR}/command"))"
+}
 
-  local FUNCTION=execute; for PARAMETER in "${COMMAND[@]}"; do
-    if [[ "$PARAMETER" == '-h' || "$PARAMETER" == '--help' ]]; then FUNCTION=help; break; fi
+# Execute command
+function execute() {
+  local COMMAND=( "$@" )
+  local COMMAND_FILE="${QUNITY_DIR}/command/${COMMAND[0]-}.sh"
+
+  # shellcheck source=./.qunity/command/*.sh
+  if [[ -f "$COMMAND_FILE" ]]; then source "$COMMAND_FILE"; fi
+
+  for PARAMETER in "${COMMAND[@]}"; do
+    if [[ "$PARAMETER" == '-h' || "$PARAMETER" == '--help' ]]; then
+      COMMAND[0]=help; break
+    fi
   done
 
-  if [[ $# -eq 0 || "$FUNCTION" == help ]]; then help; return 0; fi
+  if [[ $# -eq 0 || "${COMMAND[0]}" == help ]]; then
+    help; return 0
+  fi
 
-  print "INFO: Start '${START_MESSAGE[*]}'" 32; if execute "${COMMAND[@]}"
-    then print "${RESULT_MESSAGE="INFO: Success"}" 32
-    else print "${RESULT_MESSAGE="ERROR: Unknown error"}" 31
+  print "INFO: Start '${COMMAND[*]}'" 32;
+
+  if ( "${COMMAND[@]}" ); then
+    print "${RESULT_MESSAGE="INFO: Successful '${COMMAND[*]}"}'" 32
+  else
+    print "${RESULT_MESSAGE="ERROR: ${COMMAND[*]}"}" 31
   fi
 }
 
-start "${@:1}"
+execute "${@:1}"
