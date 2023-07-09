7a880c1e4af3334da0934360b7ab508d3af225a3
Rodion Kachkin <r.kachkin@mygento.com>
2023-07-09 15:16:17 +0400

    Create magento:download command of qunity.sh script

--- .qunity/command/magento.sh
+++ .qunity/command/magento.sh
@@ -7,8 +7,6 @@ DESK="Website Magento command group"
 HELP="$(color 32 "$DESK")\n
 $(color 33 "Usage:")\n    command [options]\n
 $(color 33 "Options:")\n    -h, --help\t\t - Display this help menu\n
-    -f, --force\t\t - Forced download of repository
-\t\t\t   WARNING: It will delete all previously unsaved data\n
 $(color 33 "Commands:")\n$(commands "./${NAME}")"
 
 EXEC=( "@replace" "execute '${NAME}' --help" )
---
+++ .qunity/command/magento/download.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+load library:download
+
+NAME="magento:download"
+DESK="Download Magento website"
+
+HELP="$(color 32 "$DESK")\n
+$(color 33 "Usage:")\n    command [options]\n
+$(color 33 "Options:")\n    -h, --help\t\t - Display this help menu\n
+    -f, --force\t\t - Forced download of repository
+\t\t\t   WARNING: It will delete all previously unsaved data\n
+$(color 33 "Commands:")\n$(commands "./${NAME}")"
+
+magento:download() {
+  download "$MAGENTO_REPOSITORY" "$MAGENTO_REPLACE_DIR" "$MAGENTO_DESTINATION_DIR" "${ARGS[@]}"
+}
