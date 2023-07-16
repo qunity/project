35d199919d21c5356f5e602907f63c32cd99c248
Rodion Kachkin <r.kachkin@mygento.com>
2023-07-16 14:31:22 +0400

    Extend 'warden:exec' function of qunity script

--- .qunity/library/warden/exec.sh
+++ .qunity/library/warden/exec.sh
@@ -4,9 +4,11 @@ warden:exec() {
   local ARGS=( "$@" )
 
   case "${ARGS[0]}" in
-    magento) ARGS[0]="/var/www/html/bin/magento" ;;
-    pwa) ARGS[0]="" ;; # TODO: make a command in /var/www/pwa via yarn
+    php) ARGS=( "php-fpm" "/usr/bin/php" "${ARGS[@]:1}" ) ;;
+    composer) ARGS=( "php-fpm" "/usr/bin/composer" "${ARGS[@]:1}" ) ;;
+    magento) ARGS=( "php-fpm" "/var/www/html/bin/magento" "${ARGS[@]:1}" ) ;;
+    pwa|yarn) ;; # TODO: make a command in /var/www/pwa via yarn
   esac
 
-  if warden env exec php-fpm "${ARGS[@]}"; then return 0; else return 1; fi
+  if warden env exec "${ARGS[@]}"; then return 0; else return 1; fi
 }
