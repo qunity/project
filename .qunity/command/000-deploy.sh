6454e42181e4098840787ce3d2cdaa476f9f0dda
Rodion Kachkin <kyleRQWS@gmail.com>
2024-02-21 19:52:55 +0400

    Fix "deploy" command of qunity script

--- .qunity/command/000-deploy.sh
+++ .qunity/command/000-deploy.sh
@@ -7,8 +7,7 @@ DESK="Deploy project for development"
 HELP="$(color 32 "$DESK")\n
 $(color 33 "Usage:")\n$(help:string "${SCRIPT} ${NAME} [options]")\n
 $(color 33 "Options:")\n$(help:string "-h, --help" "- Display this help menu")\n
-$(help:string "-f, --force" "- Forced download of repository")
-$(help:string "" "  WARNING: It will delete all previously unsaved data")"
+$(color 33 "Commands:")\n$(help:commands "$NAME")"
 
 EXEC=(
   "execute warden:up"
