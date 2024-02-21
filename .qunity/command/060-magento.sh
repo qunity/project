1a55f0b340972b9c3db40e3723ba04b905767f81
Rodion Kachkin <kyleRQWS@gmail.com>
2024-02-21 20:40:29 +0400

    Fix help information for group commands of qunity script #2

--- .qunity/command/060-magento.sh
+++ .qunity/command/060-magento.sh
@@ -5,7 +5,7 @@ NAME="magento"
 DESK="Magento website commands group"
 
 HELP="$(color 32 "$DESK")\n
-$(color 33 "Usage:")\n$(help:string "${SCRIPT} ${NAME} [options]")\n
+$(color 33 "Usage:")\n$(help:string "${SCRIPT} [command] [options]")\n
 $(color 33 "Options:")\n$(help:string "-h, --help" "- Display this help menu")\n
 $(color 33 "Commands:")\n$(help:commands "$NAME")"
 
