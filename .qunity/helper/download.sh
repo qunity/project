20580db0356eb123ea37fa11ac2c36c901da1f21
Rodion Kachkin <kyleRQWS@gmail.com>
2024-02-23 01:20:17 +0400

    Fixes 'arg:has' function qunity script Php version in base .env file Change 'color' function to 'style' function in 'download' helper

--- .qunity/helper/download.sh
+++ .qunity/helper/download.sh
@@ -8,27 +8,27 @@ download() {
   local REPLACE_DIR; REPLACE_DIR="$(realpath -m "${QUNITY_DIR}/replace/${REPOSITORY_DIR}")"
   local DESTINATION_DIR; DESTINATION_DIR="$(realpath --canonicalize-missing "${BASE_DIR}/${2}")"
 
-  print "$(color 32 "Downloading repository:") ${REPOSITORY}"
+  print "$(style 32 "Downloading repository:") ${REPOSITORY}"
 
   if [[ -d "$DESTINATION_DIR" ]] && arg:has "-f:--force" "$@"; then
-    print "$(color 33 "WARNING:") Directory will be removed: ${DESTINATION_DIR}"
+    print "$(style 33 "WARNING:") Directory will be removed: ${DESTINATION_DIR}"
 
     if ! question:yesno "Are you sure you want to continue?"; then
-      print "$(color 32 "Execution aborted")"; return 0
+      print "$(style 32 "Execution aborted")"; return 0
     fi
 
     if ! rm -rf "$DESTINATION_DIR"; then
-      print "$(color 31 "Failed to remove directory:") ${DESTINATION_DIR}"; return 1
+      print "$(style 31 "Failed to remove directory:") ${DESTINATION_DIR}"; return 1
     fi
   fi
 
   if ! git clone --single-branch "$REPOSITORY" "$DESTINATION_DIR"; then
-    print "$(color 31 "Failed to clone repository:") ${REPOSITORY} > ${DESTINATION_DIR}"; return 1
+    print "$(style 31 "Failed to clone repository:") ${REPOSITORY} > ${DESTINATION_DIR}"; return 1
   fi
 
   if [[ -d "$REPLACE_DIR" ]] && ! cp -r "$REPLACE_DIR" "$(dirname "$DESTINATION_DIR")"; then
-    print "$(color 31 "Failed to copy directory:") ${REPLACE_DIR} > ${DESTINATION_DIR}"; return 1
+    print "$(style 31 "Failed to copy directory:") ${REPLACE_DIR} > ${DESTINATION_DIR}"; return 1
   fi
 
-  print "$(color 32 "Successful complete")"
+  print "$(style 32 "Successful complete")"
 }
