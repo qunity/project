a9098c2c1e41f2849af4ddc947304f61f9a5188a
Rodion Kachkin <kyleRQWS@gmail.com>
2024-03-10 03:51:11 +0400

    Move creating magento directories to helper

---
+++ .qunity/helper/magento/create-directories.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+magento:create-directories() {
+  if ! mkdir -p "${BASE_DIR}/${WARDEN_WEB_ROOT}/app/code/Magento" \
+        "${BASE_DIR}/${WARDEN_WEB_ROOT}/lib/internal/Magento/Framework" \
+        "${BASE_DIR}/${WARDEN_WEB_ROOT}/dev/tests/static/framework/Magento/Sniffs" \
+        "${BASE_DIR}/${WARDEN_WEB_ROOT}/dev/tools/Magento/Tools" \
+        "${BASE_DIR}/${WARDEN_WEB_ROOT}/dev/build/publication/sanity/Magento/Tools/Sanity"
+  then
+      print "$(style 31 "Failed to create magento directories for development")"; return 1
+  fi
+}
