bfe026b4b5a1cddcb91d8e8bf62ae914f3c9bdac
Rodion Kachkin <kyleRQWS@gmail.com>
2024-03-10 02:50:12 +0400

    Add 'deploy:completion' function to deploy command

--- .qunity/command/010-deploy.sh
+++ .qunity/command/010-deploy.sh
@@ -12,4 +12,12 @@ $(style 33 "Options:")\n$(help:string "-h, --help" "- Display this help menu")"
 
 EXEC=( "execute warden:down --remove" "execute magento:download --force" "execute warden:up"
 "execute magento:install" "execute magento:admin:create" "execute magento:reindex" "execute magento:cache"
-"magento:urn-generate" )
+"magento:urn-generate" "deploy:completion")
+
+deploy:completion() {
+  mkdir -p "${BASE_DIR}/${WARDEN_WEB_ROOT}/app/code/Magento" \
+    "${BASE_DIR}/${WARDEN_WEB_ROOT}/lib/internal/Magento/Framework" \
+    "${BASE_DIR}/${WARDEN_WEB_ROOT}/dev/tests/static/framework/Magento/Sniffs" \
+    "${BASE_DIR}/${WARDEN_WEB_ROOT}/dev/tools/Magento/Tools" \
+    "${BASE_DIR}/${WARDEN_WEB_ROOT}/dev/build/publication/sanity/Magento/Tools/Sanity"
+}
