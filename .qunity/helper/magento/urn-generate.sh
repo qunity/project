afe8ff1a9042f4c79997aae5c7544d25fca4e906
Rodion Kachkin <kyleRQWS@gmail.com>
2024-03-10 03:37:14 +0400

    Fix code style for magento:urn-generate file helper

--- .qunity/helper/magento/urn-generate.sh
+++ .qunity/helper/magento/urn-generate.sh
@@ -3,16 +3,16 @@
 load library:warden:exec
 
 magento:urn-generate() {
-    if ! warden:exec magento dev:urn-catalog:generate --ide phpstorm misc.xml; then
-      print "$(style 31 "Failed to generate Catalog URN list for PhpStorm")"; return 1
-    fi
+  if ! warden:exec magento dev:urn-catalog:generate --ide phpstorm misc.xml; then
+    print "$(style 31 "Failed to generate Catalog URN list for PhpStorm")"; return 1
+  fi
 
-    local MAGENTO_DIR="${BASE_DIR}/${WARDEN_WEB_ROOT}"
+  local MAGENTO_DIR="${BASE_DIR}/${WARDEN_WEB_ROOT}"
 
-    # shellcheck disable=SC2016
-    if ! sed -i 's/\$PROJECT_DIR\$/\$PROJECT_DIR\$\/magento/g' "${MAGENTO_DIR}/misc.xml" ||
-       ! mv -f "${MAGENTO_DIR}/misc.xml" "${BASE_DIR}/.idea/misc.xml"
-    then
-      print "$(style 31 "Failed to generate Catalog URN list for PhpStorm")"; return 1
-    fi
+  # shellcheck disable=SC2016
+  if ! sed -i 's/\$PROJECT_DIR\$/\$PROJECT_DIR\$\/magento/g' "${MAGENTO_DIR}/misc.xml" ||
+     ! mv -f "${MAGENTO_DIR}/misc.xml" "${BASE_DIR}/.idea/misc.xml"
+  then
+    print "$(style 31 "Failed to generate Catalog URN list for PhpStorm")"; return 1
+  fi
 }
